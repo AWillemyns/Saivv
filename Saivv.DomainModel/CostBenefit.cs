b58a9075046183e58256b7a678ca0dbc5c0d494c
Arnesaurus <arne.willemyns@gmail.com>
2019-03-05 09:07:33 +0100

    Initial commit

---
+++ Saivv.DomainModel/CostBenefit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saivv.DomainModel
+{
+    public class CostBenefit
+    {
+        public decimal Cost { get; set; }
+        public decimal Benefit { get; set; }
+
+        public decimal GetCostBenefitRatio()
+        {
+            return this.Benefit / this.Cost;
+        }
+    }
+}
---
+++ Testing/Saivv.DomainModel.UnitTests/CostBenefitTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Saivv.DomainModel.UnitTests
+{
+    [TestFixture]
+    public class CostBenefitTests
+    {
+        public CostBenefit CostBenefit { get; set; }
+        [SetUp]
+        public void Setup()
+        {
+            this.CostBenefit = new CostBenefit();
+        }
+        [Test]
+        public void CreateCostBenefit()
+        {
+            Assert.IsNotNull(this.CostBenefit);
+        }
+
+        [Test]
+        public void TestCostBenefitRatio()
+        {
+            this.CostBenefit.Cost = 250;
+            this.CostBenefit.Benefit = 500;
+            decimal costBenefitRatio = this.CostBenefit.GetCostBenefitRatio();
+            Assert.AreEqual(2, costBenefitRatio);
+        }
+
+    }
+
+
+
+}
